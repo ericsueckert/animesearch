de9ce069f0b0807af3985021ae6d4295bfdf9006
Eric Eckert <eric95@uw.edu>
2016-12-15 05:38:48 -0800

    autocomplete still working

--- script/search.js
+++ script/search.js
@@ -1,4 +1,4 @@
-var animeApp = angular.module('AnimeSearchApp', ['autocomplete']);
+var animeApp = angular.module('AnimeSearchApp', ['autocomplete', 'ngSanitize']);
 
 animeApp.factory('AnimeRetriever', function ($http, $timeout) {
     var AnimeRetriever = new Object();
@@ -6,19 +6,17 @@ animeApp.factory('AnimeRetriever', function ($http, $timeout) {
     AnimeRetriever.updateAnime = function (typed) {
         var anime;
 
-        console.log("Typed: " + typed);
-        console.log("HTTP request to AnimeNewsNetwork");
+        // console.log("Typed: " + typed);
+        // console.log("HTTP request to AnimeNewsNetwork");
 
         //HTTP GET request to animenewsnetwork with delay
-        var promise = $timeout(function () {
+        //return promise
+        return $timeout(function () {
             return $http({
                 method: 'GET',
                 url: 'https://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=~' + typed
             });
         }, 1000);
-
-        //return promise
-        return promise;
 
 //                     // console.log("Posting data to Python script...");
 //                     // $timeout(function () {
@@ -42,21 +40,22 @@ animeApp.factory('AnimeRetriever', function ($http, $timeout) {
 
     //Query an anime title
     AnimeRetriever.query = function (text) {
-        searchTitle = string.split('(')[0];
-        var promise = $timeout(function () {
+        console.log(text);
+        searchTitle = text.split('\t')[0];
+        console.log(searchTitle);
+        return $timeout(function () {
             return $http({
                 method: 'GET',
                 url: 'https://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=~' + searchTitle
             });
         }, 1000);
-        return promise;
-    }
+    };
 
     return AnimeRetriever;
 });
 
 //Controller
-animeApp.controller('AnimeCtrl', ['$scope', 'AnimeRetriever', function ($scope, AnimeRetriever) {
+animeApp.controller('AnimeCtrl', ['$scope', '$sce', 'AnimeRetriever', function ($scope, $sce, AnimeRetriever) {
 
     $scope.getAnime = function () {
         return $scope.animeTitles;
@@ -71,18 +70,18 @@ animeApp.controller('AnimeCtrl', ['$scope', 'AnimeRetriever', function ($scope, 
         //Submit request
         $scope.newanime = AnimeRetriever.updateAnime(typed);
 
-        console.log($scope.newanime);
+        // console.log($scope.newanime);
 
         //Handle request result
         $scope.newanime.then(function (response) {
             //Successful response
-            console.log("HTTP GET request successful.");
+            // console.log("HTTP GET request successful.");
             var anime = response.data;
 //                    console.log(anime);
             //Convert to JSON format
             var x2js = new X2JS();
             var jsonAnime = x2js.xml_str2json(anime);
-            console.log(jsonAnime.ann.anime);
+            // console.log(jsonAnime.ann.anime);
             $scope.animeData = jsonAnime.ann.anime;
 
             var titleList = [];
@@ -92,12 +91,12 @@ animeApp.controller('AnimeCtrl', ['$scope', 'AnimeRetriever', function ($scope, 
 
                 anime = jsonAnime.ann.anime[i];
                 //console.log(anime);
-                var searchLine = anime._name + " (" + anime._type + ")";
-                console.log(searchLine);
+                var searchLine = anime._name + "\t(" + anime._precision + ")";
+                // console.log(searchLine);
                 titleList.push(searchLine);
             }
-            console.log("return anime");
-            console.log(titleList);
+            // console.log("return anime");
+            // console.log(titleList);
             //$scope.animeTitles = ["Shingeki no Kyojin Kōhen: Jiyū no Tsubasa (movie)", "Shingeki no Kyojin Zenpen: Guren no Yumiya (movie)"];
 
             //Update $scope variable with titles
@@ -106,19 +105,57 @@ animeApp.controller('AnimeCtrl', ['$scope', 'AnimeRetriever', function ($scope, 
         }, function (response) {
             //Failure response
             alert('Failed: ' + response.statusText);
-            return;
         });
 
     };
 
     $scope.searchAnime = function (suggestion) {
+        console.log("Querying anime: " + suggestion);
         $scope.queryResult = AnimeRetriever.query(suggestion);
+        splitSuggestion = suggestion.split('\t');
+        splitSuggestion[1] = splitSuggestion[1].replace('(','').replace(')','');
+        console.log(splitSuggestion[1]);
+
         $scope.queryResult.then(function (response) {
             console.log("Query successful");
-            anime = response.data;
+            animeResponse = response.data;
             var x2js = new X2JS();
-            var jsonAnime = x2js.xml_str2json(anime);
-            var anime = jsonAnime.ann.anime[0];
+            var jsonAnime = x2js.xml_str2json(animeResponse);
+            console.log(jsonAnime);
+            var anime = jsonAnime.ann.anime;
+            if (angular.isArray(anime)) {
+                for (var i = 0; i < anime.length; i++) {
+                    if (anime[i]._name == splitSuggestion[0] && anime[i]._precision == splitSuggestion[1]) {
+                        console.log("correctly identified");
+                        anime = anime[i];
+                        break;
+                    }
+                }
+            }
+            console.log(anime);
+
+            delete $scope.header;
+            delete $scope.picture;
+            delete $scope.summary;
+
+            console.log("binding header");
+            // $scope.header = $sce.trustAsHtml("<h3>" + anime._name + "</h3>" +
+            //     "<h4>" + anime._type + "</h4>");
+            $scope.header = $sce.trustAsHtml("Header");
+            console.log($scope.header);
+            var type;
+            for (var i = 0; i < anime.length; i++) {
+                type = anime[i].type;
+                if (type == "Picture") {
+                    console.log("binding image");
+                    $scope.picture = $sce.trustAsHtml(anime[i].src);
+                }
+                else if (type == "Plot Summary") {
+                    $scope.summary = $sce.trustAsHtml(anime[i]);
+                }
+            }
+
+
         })
     };
 }]);
